f6679b309708e7ca64b01ca43c130b23f0266e4a
metaphysica1412 <yl15@princeton.edu>
2018-10-01 23:29:41 -0400

    Oct 1, 2018

---
+++ Code/str_format.py
@@ -0,0 +1,14 @@
+age = 20
+name = 'Swaroop'
+
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
+
+print('{0:.3f}'.format(1.0/3))
+print('{0:_^11}'.format('hello'))
+print('{name} wrote {book}'.format(book = 'A Byte of Python', name = 'Swaroop'))
+
+print('a', end ='')
+print('b')
+
+print(r'Newlines are indicated by \n')
